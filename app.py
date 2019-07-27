0e21c06627c4ae145c0207cc4af4cd6a5c4a34c6
SteveImmanuel <steveandreasimanuel@gmail.com>
2019-07-27 09:46:14 +0700

    debug #4

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
-channel_secret = '4d29fcb0bc85d329492c2fc90fe9986c'
-channel_access_token = 'Jj/jv56P7ZEREEIQ5zZ6SmSYksi14BFFLyFDSPjIBxjP4At1nGzaB6PAPxTux55BVVBGC8+SA1aHssHTVfVs3EgADnOIGqWwPuVWay01R/qP9XVv5wy60dmzKIEP3nrJYa4waIfzrHW+bddcYrfJxgdB04t89/1O/w1cDnyilFU='
+channel_secret = os.getenv('CHANNEL_SECRET', None)
+channel_access_token = os.getenv('CHANNEL_ACCESS', None)
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 tz=pytz.timezone('Asia/Jakarta')
@@ -21,20 +21,20 @@ app = Flask(__name__)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-	# get X-Line-Signature header value
-	signature = request.headers['X-Line-Signature']
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-	# get request body as text
-	body = request.get_data(as_text=True)
-	app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-	# handle webhook body
-	try:
-		handler.handle(body, signature)
-	except InvalidSignatureError:
-		abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-	return 'OK'
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -43,8 +43,6 @@ def handle_message(event):
 	line_bot_api.reply_message(event.reply_token,TextSendMessage(text=str(now)))
 
 if __name__ == "__main__":
-	print(channel_access_token)
-	print(channel_secret)
 	app.run()
 	
 
